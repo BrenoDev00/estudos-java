b15e5642f87f1bd088da6931a6352ac10f07554b
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-02 15:56:29 -0300

    Aprendendo sobre os tipos primitivos

---
+++ anotacoes/TiposPrimitivos.java
@@ -0,0 +1,44 @@
+package anotacoes;
+
+public class TiposPrimitivos{
+    public static void numericosInteiros(String[] args){
+        //Tipos numéricos inteiros
+        
+        // byte
+        byte myByte = 122;
+
+        //short 
+        short myShort = 23000;
+
+        //int
+        int myInt = 56789;
+
+        //long
+        long myLong = 45346654;
+    }
+
+    public static void numericosFlutuantes(String[] args){
+        //Tipos numéricos flutuantes
+
+        //float
+        float myFloat = 3.14f;
+
+        //double 
+        double myDouble = 45.689;
+    }
+
+    public static void booleano(String[] args){
+        //Booleano
+        boolean myBool = true;
+
+        boolean myOtherBool = false;
+    }
+
+    public static void caracteres(String[] args){
+        //char: um caractere unicode
+        char myChar = 'B';
+
+        // String: conjunto de caracteres
+        String myString = "Breno";
+    }
+}
---
+++ anotacoes/Variaveis.java
@@ -0,0 +1,16 @@
+package anotacoes;
+
+public class Variaveis{
+    public static void main (String[] args){
+        // Sintaxe de declaração de variáveis no Java
+        // <tipo> <nome> = <valor inicial> (opcional)
+
+        int userAge = 23;
+
+        String userName = "Jhon Doe";
+
+        double accountNumber = 23.343;
+ 
+        char gender = 'M';
+    }
+}
