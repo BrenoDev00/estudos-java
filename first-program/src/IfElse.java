d35213a0db39319f4f8da3ce5ebbde7d241403ee
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-09 12:09:36 -0300

    aprendendo conidicional if else

---
+++ first-program/src/IfElse.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class IfElse{
+    public static void main(String[] args){
+        // condicional if else
+        // estrutura:
+
+        int number = 3;
+
+        if(number > 10){
+            System.out.println("número é maior que 10");
+        }else {
+            System.out.println("número é menor que 10");
+        }
+        example();
+        
+    }
+
+    public static void example(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Informe a hora: ");
+        double time = sc.nextDouble();
+
+        if(time < 13){
+            System.out.println("Agora é dia");
+        }else if(time < 19){
+                System.out.println("Agora é tarde");
+        }else {
+                System.out.println("Agora é noite");
+            }
+        
+
+        sc.close();
+    }
+}
+
